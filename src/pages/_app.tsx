5eecd7e358919571ba99d866d5456d740578b784
techysuite <63807149+angelbendgr8-dev@users.noreply.github.com>
2023-11-03 13:38:28 +0100

    updated node version

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,7 +12,7 @@ import {
 } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:3000/graphql",
+  uri: "https://scrapay-backend.onrender.com/graphql",
   cache: new InMemoryCache(),
 });
 
